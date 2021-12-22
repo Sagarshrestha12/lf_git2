6fc6ee98592df294a8d958460cc00a60cc338c6f
Sagarshrestha12 <sagarshrestha4339@gmail.com>
2021-12-22 09:22:21 +0545

    Added the sorting of object using specific key

--- assignment/jsexercise/src/sorting_arr.js
+++ assignment/jsexercise/src/sorting_arr.js
@@ -11,36 +11,51 @@ var arr = [
     id: 3,
     name: "Andrew",
   },
+  {
+    id: 5,
+    name: "Bishal",
+  },
 ];
-function partition(array, start_index, end_index) {
-  let pivot = array[start_index];
+function partition(array, start_index, end_index, key) {
+  let pivot = array[start_index][key];
   let st = start_index;
   let lt = end_index;
   while (st < lt) {
-    while (array[st] <= pivot) {
+    while (array[st][key] <= pivot) {
       st += 1;
     }
-    while (array[lt] > pivot) {
+    while (array[lt][key] > pivot) {
       lt -= 1;
     }
     if (st < lt) {
-      [array[st], array[lt]] = [array[lt], array[st]];
+      [array[st][key], array[lt][key]] = [array[lt][key], array[st][key]];
     }
   }
   st -= 1;
-  [array[start_index], array[st]] = [array[st], array[start_index]];
+  [array[start_index][key], array[st][key]] = [
+    array[st][key],
+    array[start_index][key],
+  ];
   return st;
 }
-function quicksort_Child(array, start, end) {
+function quicksort_Child(array, start, end, key) {
   if (start < end) {
-    index = partition(array, start, end);
-    quicksort_Child(array, start, index - 1);
-    quicksort_Child(array, index + 1, end);
+    index = partition(array, start, end, key);
+    quicksort_Child(array, start, index - 1, key);
+    quicksort_Child(array, index + 1, end, key);
   } else {
     return 1;
   }
 }
-function quicksort(array) {
-  quicksort_Child(array, 0, array.length - 1);
+function quicksort(array, key) {
+  quicksort_Child(array, 0, array.length - 1, key);
 }
 
+function sortBy(array, key) {
+  // for (let obj in array){
+  let arr = [...array];
+  quicksort(arr, key);
+  return arr;
+}
+// console.log(arr[0]["id"]);
+console.log(sortBy(arr, "name"));
