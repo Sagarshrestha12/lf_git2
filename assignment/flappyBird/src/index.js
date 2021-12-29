f1f175c8ed97e81a6b53faa9249ca34d3a1fc751
Sagarshrestha12 <sagarshrestha4339@gmail.com>
2021-12-30 00:47:26 +0545

    Loaded the images using Promises

--- assignment/flappyBird/src/index.js
+++ assignment/flappyBird/src/index.js
@@ -1,7 +1,31 @@
 const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerWidth;
-const ctx = canvas.getContext();
-imagesLocation=[
+canvas.width = 500;
+canvas.height = 730;
+canvas.style.border = "3px solid black";
+let ctx = canvas.getContext("2d");
+const imagesLocation = [
+  "../images/bottom-background.png",
+  "../images/fb-game-background.png",
+  "../images/flappy-bird.png",
+  "../images/flappybird-pipe.png",
+];
+ctx.arc(20, 20, 0, 2 * Math.PI, true);
 
-]
+const images = []; // consist of images that is loaded asynchronously
+const loadImage = (url) =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener("load", () => resolve(img));
+    img.addEventListener("error", (err) => reject(err));
+    img.src = url;
+  });
+
+let promis = Promise.all(imagesLocation.map(loadImage))
+  .then((img) => {
+    images = img;
+  })
+  .catch((err) => console.log("Error in loading images"));
+
+promis.then(() => {
+  ctx.drawImage(images[0], 0, 0);
+});
--- assignment/flappyBird/src/utils.js
+++ assignment/flappyBird/src/utils.js
@@ -4,3 +4,6 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+a=[1,2,3]
+b=a
+console.log(b)
