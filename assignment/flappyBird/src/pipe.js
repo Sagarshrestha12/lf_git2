c31cff44391a73133b58651867c226dfe9bfbe44
Sagarshrestha12 <sagarshrestha4339@gmail.com>
2021-12-30 09:20:48 +0545

    added the additional images and pipe implemetation

--- assignment/flappyBird/src/pipe.js
+++ assignment/flappyBird/src/pipe.js
@@ -1,9 +1,24 @@
 class Pipe {
-  constructor(x, y) {
+  constructor(image) {
     this.speed = 5;
+    this.height = 300;
+    this.gap = 150;
+    this.image = image;
+    this.shift = 2;
   }
-  move() {
-    this.x = this.x - this.speed;
-  }
-
+  draw = function (pipes) {
+    for (let i = 0; i < pipes.length; i++) {
+      let topPipe = pipes[i].y;
+      let buttomPipe = pipes[i].y + this.height + this.gap;
+      ctx.drawImage(this.image[1], pipes[i].x, topPipe); //top pipe
+      ctx.drawImage(this.image[0], pipes[i].x, buttomPipe); //bottom pipe
+      pipes[i].x -= this.shift;
+      if (pipes[i].x === 300) {
+        pipes.push({
+          x: 500,
+          y: getRandomNumber(-this.height + 200, 0),
+        });
+      }
+    }
+  };
 }
